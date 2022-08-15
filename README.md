ac05c5d69c55921a29226bc620f517860d0dd543
tesso57 <tesso1729@gmail.com>
2022-08-16 02:43:41 +0900

    ステップ1

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # zig-cc
 9ccのzig版
+
+- [9cc について](https://www.sigbus.info/compilerbook)
+
+## usefull web sites
+### zig
+- [https://ziglearn.org/](https://ziglearn.org/)
+
+### c compailer 
+- [https://godbolt.org/](https://godbolt.org/)
--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,28 @@
 const std = @import("std");
+fn countChars(chars: [*:0]u8) usize {
+    var i: usize = 0;
+    while (true) {
+        if (chars[i] == 0) {
+            return i;
+        }
+        i += 1;
+    }
+}
 
-pub fn main() void {
-    std.debug.print("Hello, {s}!\n", .{"World"});
+fn getArgv() i32 {
+    const arg1: [*:0]u8 = std.os.argv[1];
+    const arg1_unmodifiable_slice: []const u8 = arg1[0..countChars(arg1)];
+    const n: i32 = std.fmt.parseInt(i32, arg1_unmodifiable_slice, 10) catch 0;
+    return n;
+}
+
+pub fn main() !void {
+    const argv = getArgv();
+    // std.debug.print("{!}", .{argv});
+    const stdout = std.io.getStdOut().writer();
+    try stdout.print(".intel_syntax noprefix\n", .{});
+    try stdout.print(".global main\n", .{});
+    try stdout.print("main:\n", .{});
+    try stdout.print("   mov rax, {!}\n", .{argv});
+    try stdout.print("   ret\n", .{});
 }
