fe92f551d8c69fa798cf5afb8b4999686d528b4e
tesso57 <tesso1729@gmail.com>
2022-08-21 16:43:37 +0900

    update error message

--- src/main.zig
+++ src/main.zig
@@ -22,13 +22,45 @@ const Token = struct {
     kind: TokenKind,
     next: ?*Token,
     val: i32,
-    char: u8,
+    pos: usize,
 };
 
 var token: ?*Token = null;
+var input: []const u8 = undefined;
+
+fn isSpace(char: u8) bool {
+    return char == ' ' or char == '\t' or char == '\n';
+}
+
+fn isDigit(char: u8) bool {
+    return '0' <= char and char <= '9';
+}
+
+fn getInt(start: *usize) i32 {
+    var end: usize = start.* + 1;
+    while (input[end] != 0) : (end += 1) {
+        if (!isDigit(input[end])) break;
+    }
+    const ret = std.fmt.parseInt(i32, input[start.*..end], 10) catch 0;
+    start.* = end;
+    return ret;
+}
+
+fn errorAt(loc: usize, string: []const u8) !void {
+    try stderr.writer().print("{s}\n", .{input});
+    {
+        var i: usize = 0;
+        while (i != loc) : (i += 1) {
+            try stderr.writer().print("{c}", .{' '});
+        }
+    }
+    try stderr.writer().print("{s}", .{"^ "});
+    try stderr.writer().print("{s}\n", .{string});
+    exit(1);
+}
 
 fn consume(op: u8) bool {
-    if (token.?.kind != TokenKind.TK_RESERVED or token.?.char != op) {
+    if (token.?.kind != TokenKind.TK_RESERVED or input[token.?.pos] != op) {
         return false;
     }
     token = token.?.next;
@@ -36,8 +68,8 @@ fn consume(op: u8) bool {
 }
 
 fn expect(op: u8) !void {
-    if (token.?.kind != TokenKind.TK_RESERVED or token.?.char != op) {
-        try stderr.writeAll("expect error\n");
+    if (token.?.kind != TokenKind.TK_RESERVED or input[token.?.pos] != op) {
+        try errorAt(token.?.pos, "expect error");
         exit(1);
     }
     token = token.?.next;
@@ -45,7 +77,7 @@ fn expect(op: u8) !void {
 
 fn expectNumber() !i32 {
     if (token.?.kind != TokenKind.TK_NUM) {
-        try stderr.writeAll("数ではない\n");
+        try errorAt(token.?.pos, "数ではない");
         exit(1);
     }
 
@@ -58,58 +90,37 @@ fn atEof() bool {
     return token.?.kind == TokenKind.TK_EOF;
 }
 
-fn newToken(kind: TokenKind, cur: *Token, char: u8) !*Token {
+fn newToken(kind: TokenKind, cur: *Token, pos: usize) !*Token {
     const tok = try allocator.create(Token);
     tok.kind = kind;
-    tok.char = char;
+    tok.pos = pos;
     cur.next = tok;
     return tok;
 }
 
-fn isSpace(char: u8) bool {
-    return char == ' ' or char == '\t' or char == '\n';
-}
-
-fn isDigit(char: u8) bool {
-    return '0' <= char and char <= '9';
-}
-
-fn getInt(p: []const u8, start: *usize) i32 {
-    var end = start.* + 1;
-    while (p[end] != 0) : (end += 1) {
-        if (!isDigit(p[end])) break;
-    }
-    const ret = std.fmt.parseInt(i32, p[start.*..end], 10) catch 0;
-    start.* = end;
-    return ret;
-}
-
-fn tokenize(p: []const u8) !*Token {
-    var head: ?Token = Token{ .kind = TokenKind.TK_RESERVED, .next = null, .val = 0, .char = ' ' };
+fn tokenize(p: *const []const u8) !*Token {
+    var head: ?Token = Token{ .kind = TokenKind.TK_RESERVED, .next = null, .val = 0, .pos = 0 };
     var cur = &(head.?);
-
     var i: usize = 0;
-    while (p[i] != 0) : (i += 1) {
-        if (isSpace(p[i]))
+    while (p.*[i] != 0) : (i += 1) {
+        if (isSpace(p.*[i]))
             continue;
 
-        if (p[i] == '+' or p[i] == '-') {
-            cur = try newToken(TokenKind.TK_RESERVED, cur, p[i]);
+        if (p.*[i] == '+' or p.*[i] == '-') {
+            cur = try newToken(TokenKind.TK_RESERVED, cur, i);
             continue;
         }
 
-        if (isDigit(p[i])) {
-            cur = try newToken(TokenKind.TK_NUM, cur, p[i]);
-            cur.val = getInt(p, &i);
+        if (isDigit(p.*[i])) {
+            cur = try newToken(TokenKind.TK_NUM, cur, i);
+            cur.val = getInt(&i);
             i -= 1;
             continue;
         }
-
-        try stderr.writeAll("トークナイズ失敗\n");
+        try errorAt(i, "トークナイズできない");
         exit(1);
     }
-
-    _ = try newToken(TokenKind.TK_EOF, cur, '\n');
+    _ = try newToken(TokenKind.TK_EOF, cur, 0);
     return head.?.next.?;
 }
 
@@ -123,8 +134,8 @@ pub fn main() !void {
 
     var i: usize = 0;
     while (std.os.argv[1][i] != 0) : (i += 1) {}
-    const input: []const u8 = std.os.argv[1][0 .. i + 1];
-    token = try tokenize(input);
+    input = std.os.argv[1][0 .. i + 1];
+    token = try tokenize(&input);
     try stdout.writeAll(".intel_syntax noprefix\n");
     try stdout.writeAll(".globl main\n");
     try stdout.writeAll("main:\n");
